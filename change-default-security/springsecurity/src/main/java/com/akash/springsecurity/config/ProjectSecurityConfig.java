acfea77ae8a45d23df935f2688024a30dd1a05a8
Akash <akash.bhuiyan@gmail.com>
2024-08-26 13:39:08 +0600

    Demo of CompromisedPasswordChecker which is supported from Spring Security 6.3 version

--- change-default-security/springsecurity/src/main/java/com/akash/springsecurity/config/ProjectSecurityConfig.java
+++ change-default-security/springsecurity/src/main/java/com/akash/springsecurity/config/ProjectSecurityConfig.java
@@ -2,15 +2,16 @@ package com.akash.springsecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -35,13 +36,22 @@ public class ProjectSecurityConfig {
 
     @Bean
     public UserDetailsService userDetailsService() {
-        UserDetails user = User.withUsername("user").password("{noop}12345").authorities("read").build(); // noop means, I don't want to use any password encoder. So the password will be in plain text
-        UserDetails admin = User.withUsername("admin").password("{bcrypt}$2a$12$liRSqZOW4fR0zpWwC.B56ef6N85mj3zLYjUVYekw3B7mznT8cnaJG").authorities("admin").build();
+        UserDetails user = User.withUsername("user").password("{noop}Security#12345").authorities("read").build(); // noop means, I don't want to use any password encoder. So the password will be in plain text
+        UserDetails admin = User.withUsername("admin").password("{bcrypt}$2a$12$j7moSl.AHHwZUvAO/CJeQOw9NK7AYj9iriuB4V0spumDqV/11ZG1.").authorities("admin").build();
         return new InMemoryUserDetailsManager(user, admin);
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    /***
+     * From Spring Security 6.3 version
+     * @return
+     */
+    @Bean
+    public CompromisedPasswordChecker compromisedPasswordChecker() {
+        return new HaveIBeenPwnedRestApiPasswordChecker();
     }
 }
