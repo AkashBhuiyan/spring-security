8d9741ec7cf1be317525fa974e56e68ea2a2e6d6
Akash <akash.bhuiyan@gmail.com>
2024-08-26 13:08:32 +0600

    Configuring users using InMemoryUserDetailsManager

--- change-default-security/springsecurity/src/main/java/com/akash/springsecurity/config/ProjectSecurityConfig.java
+++ change-default-security/springsecurity/src/main/java/com/akash/springsecurity/config/ProjectSecurityConfig.java
@@ -3,6 +3,10 @@ package com.akash.springsecurity.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -21,8 +25,15 @@ public class ProjectSecurityConfig {
         /*http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());*/
         http.authorizeHttpRequests((requests) -> requests.requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
                 .requestMatchers("/notices","/contact","/error"));
-        http.formLogin(httpSecurityFormLoginConfigurer -> httpSecurityFormLoginConfigurer.disable());
-        http.httpBasic(httpSecurityHttpBasicConfigurer -> httpSecurityHttpBasicConfigurer.disable());
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("user").password("{noop}12345").authorities("read").build(); // noop means, I don't want to use any password encoder. So the password will be in plain text
+        UserDetails admin = User.withUsername("admin").password("{noop}54321").authorities("admin").build();
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
