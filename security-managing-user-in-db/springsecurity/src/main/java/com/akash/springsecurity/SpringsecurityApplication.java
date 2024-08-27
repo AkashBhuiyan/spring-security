506b13f51ee041eaf8db79b1b61cb519e050c1e3
Akash <akash.bhuiyan@dmoney.com.local>
2024-08-27 18:18:11 +0600

    repository and entity added

--- security-managing-user-in-db/springsecurity/src/main/java/com/akash/springsecurity/SpringsecurityApplication.java
+++ security-managing-user-in-db/springsecurity/src/main/java/com/akash/springsecurity/SpringsecurityApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+/*@EnableJpaRepositories("com.eazybytes.repository")
+@EntityScan("com.eazybytes.model")*/
 // @ComponentScan("com.akash.springsecurity.controller") //If using a different location from the main package, define the location here.
 public class SpringsecurityApplication {
 
--- security-managing-user-in-db/springsecurity/src/main/resources/sql/scripts.sql
+++ security-managing-user-in-db/springsecurity/src/main/resources/sql/scripts.sql
@@ -2,10 +2,10 @@ create table users(username varchar(50) not null primary key,password varchar(50
 create table authorities (username varchar(50) not null,authority varchar(50) not null,constraint fk_authorities_users foreign key(username) references users(username));
 create unique index ix_auth_username on authorities (username,authority);
 
-INSERT IGNORE INTO `users` VALUES ('user', '{noop}Security@12345', '1');
+INSERT IGNORE INTO `users` VALUES ('user', '{noop}kothinPass#2255', '1');
 INSERT IGNORE INTO `authorities` VALUES ('user', 'read');
 
-INSERT IGNORE INTO `users` VALUES ('admin', '{bcrypt}$2a$12$j7moSl.AHHwZUvAO/CJeQOw9NK7AYj9iriuB4V0spumDqV/11ZG1.', '1');
+INSERT IGNORE INTO `users` VALUES ('admin', '{bcrypt}$2a$12$vD9UitYrKYo/KxcFqsx/D.Fk5A0pHGzMlVK.Zp4gO5IujicEyPgf6', '1');
 INSERT IGNORE INTO `authorities` VALUES ('admin', 'admin');
 
 CREATE TABLE `customer` (
@@ -16,5 +16,5 @@ CREATE TABLE `customer` (
                             PRIMARY KEY (`id`)
 );
 
-INSERT  INTO `customer` (`email`, `pwd`, `role`) VALUES ('happy@example.com', '{noop}Security@12345', 'read');
-INSERT  INTO `customer` (`email`, `pwd`, `role`) VALUES ('admin@example.com', '{bcrypt}$2a$12$j7moSl.AHHwZUvAO/CJeQOw9NK7AYj9iriuB4V0spumDqV/11ZG1.', 'admin');
+INSERT  INTO `customer` (`email`, `pwd`, `role`) VALUES ('happy@example.com', '{noop}kothinPass#2255', 'read');
+INSERT  INTO `customer` (`email`, `pwd`, `role`) VALUES ('admin@example.com', '{bcrypt}$2a$12$vD9UitYrKYo/KxcFqsx/D.Fk5A0pHGzMlVK.Zp4gO5IujicEyPgf6', 'admin');
