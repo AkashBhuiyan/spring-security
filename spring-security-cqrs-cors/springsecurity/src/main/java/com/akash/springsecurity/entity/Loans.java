c76517e0708f1161351c7085b8cff7a5209e7b77
Akash <akash.bhuiyan@dmoney.com.local>
2024-09-05 16:50:24 +0600

    Loans entity correction for id GeneratedValue

--- spring-security-cqrs-cors/springsecurity/src/main/java/com/akash/springsecurity/entity/Loans.java
+++ spring-security-cqrs-cors/springsecurity/src/main/java/com/akash/springsecurity/entity/Loans.java
@@ -2,6 +2,8 @@ package com.akash.springsecurity.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -20,6 +22,7 @@ import java.sql.Date;
 public class Loans {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "loan_number")
     private long loanNumber;
 
