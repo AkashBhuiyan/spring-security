b930147113c3a602e2563698dbdac83a2387de78
Akash <akash.bhuiyan@dmoney.com.local>
2024-09-10 14:33:39 +0600

    Add JWTTokenValidatorFilter to validate JWT tokens before authentication
    - Implemented JWTTokenValidatorFilter to validate JWT tokens from request headers.
    - The filter decodes the JWT, extracts username and authorities, and sets the Authentication object in the SecurityContext.
    - Throws BadCredentialsException for invalid tokens.
    - The filter is applied before BasicAuthenticationFilter and skips validation for the `/user` endpoint.

--- token-based-authentication-jwt/springsecurity/src/main/java/com/akash/springsecurity/filter/JWTTokenValidatorFilter.java
+++ token-based-authentication-jwt/springsecurity/src/main/java/com/akash/springsecurity/filter/JWTTokenValidatorFilter.java
@@ -1,12 +1,24 @@
 package com.akash.springsecurity.filter;
 
+import com.akash.springsecurity.constants.ApplicationConstants;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import javax.crypto.SecretKey;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 /***
  * Akash Bhuiyan, 10/9/24
@@ -14,20 +26,33 @@ import java.io.IOException;
 
 
 public class JWTTokenValidatorFilter extends OncePerRequestFilter {
-    /**
-     * Same contract as for {@code doFilter}, but guaranteed to be
-     * just invoked once per request within a single request thread.
-     * See {@link #shouldNotFilterAsyncDispatch()} for details.
-     * <p>Provides HttpServletRequest and HttpServletResponse arguments instead of the
-     * default ServletRequest and ServletResponse ones.
-     *
-     * @param request
-     * @param response
-     * @param filterChain
-     */
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String jwt = request.getHeader(ApplicationConstants.JWT_HEADER);
+        if(null != jwt) {
+            try {
+                Environment env = getEnvironment();
+                if (null != env) {
+                    String secret = env.getProperty(ApplicationConstants.JWT_SECRET_KEY,
+                            ApplicationConstants.JWT_SECRET_DEFAULT_VALUE);
+                    SecretKey secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+                    if(null !=secretKey) {
+                        Claims claims = Jwts.parser().verifyWith(secretKey)
+                                .build().parseSignedClaims(jwt).getPayload();
+                        String username = String.valueOf(claims.get("username"));
+                        String authorities = String.valueOf(claims.get("authorities"));
+                        Authentication authentication = new UsernamePasswordAuthenticationToken(username, null,
+                                AuthorityUtils.commaSeparatedStringToAuthorityList(authorities));
+                        SecurityContextHolder.getContext().setAuthentication(authentication);
+                    }
+                }
 
+            } catch (Exception exception) {
+                throw new BadCredentialsException("Invalid Token received!");
+            }
+        }
+        filterChain.doFilter(request,response);
     }
 
     @Override
