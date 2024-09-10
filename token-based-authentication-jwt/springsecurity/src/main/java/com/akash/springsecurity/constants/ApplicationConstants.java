bcdf3a5fcdee66df278c4538cf9a223954bf795f
Akash <akash.bhuiyan@dmoney.com.local>
2024-09-10 12:54:09 +0600

    Add JWTTokenGeneratorFilter to create and attach JWT tokens to responses after authentication
    - Implemented JWTTokenGeneratorFilter extending OncePerRequestFilter to generate JWT tokens after successful authentication.
    - JWT token includes claims for username and authorities, and is signed with a secret key.
    - Added filter to set the JWT token in the response header for authenticated users.
    - The filter is applied only for requests to the `/user` endpoint and is executed after BasicAuthenticationFilter.

---
+++ token-based-authentication-jwt/springsecurity/src/main/java/com/akash/springsecurity/constants/ApplicationConstants.java
@@ -0,0 +1,12 @@
+package com.akash.springsecurity.constants;
+
+/***
+ * Akash Bhuiyan, 10/9/24
+ **/
+
+public final class ApplicationConstants {
+
+    public static final String JWT_SECRET_KEY = "JWT_SECRET";
+    public static final String JWT_SECRET_DEFAULT_VALUE = "jxgEQeXHuPq8VdbyYFNkANdudQ53YUn4";
+    public static final String JWT_HEADER = "Authorization";
+}
