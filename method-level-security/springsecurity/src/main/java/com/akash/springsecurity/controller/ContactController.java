f0ad35accbe11418f62390184e3e5f6e6f8641a5
Akash <akash.bhuiyan@dmoney.com.local>
2024-09-11 17:36:49 +0600

    Add @PreFilter annotation to filter contact inquiries before saving. Demo of @PreFilter
    - Implemented @PreFilter on the /contact endpoint to filter out contacts where contactName is 'Test'.
    - Ensured only valid contact inquiries are processed and saved in the system.
    - Demonstrates how to apply filtering logic to request data using method-level security.

--- method-level-security/springsecurity/src/main/java/com/akash/springsecurity/controller/ContactController.java
+++ method-level-security/springsecurity/src/main/java/com/akash/springsecurity/controller/ContactController.java
@@ -3,12 +3,14 @@ package com.akash.springsecurity.controller;
 import com.akash.springsecurity.entity.Contact;
 import com.akash.springsecurity.repository.ContactRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreFilter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.Date;
+import java.util.List;
 import java.util.Random;
 
 /***
@@ -22,10 +24,16 @@ public class ContactController {
     private final ContactRepository contactRepository;
 
     @PostMapping("/contact")
-    public Contact saveContactInquiryDetails(@RequestBody Contact contact) {
-        contact.setContactId(getServiceReqNumber());
-        contact.setCreateDt(new Date(System.currentTimeMillis()));
-        return contactRepository.save(contact);
+    @PreFilter("filterObject.contactName!= 'Test'")
+    public Contact saveContactInquiryDetails(@RequestBody List<Contact> contacts) {
+        if(!contacts.isEmpty()) {
+            Contact contact = contacts.getFirst();
+            contact.setContactId(getServiceReqNumber());
+            contact.setCreateDt(new Date(System.currentTimeMillis()));
+            return contactRepository.save(contact);
+        } else {
+            return null;
+        }
     }
 
     public String getServiceReqNumber() {
