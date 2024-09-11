574c7da70875a4deea15ea6c1dafb71f23a1e4bd
Akash <akash.bhuiyan@dmoney.com.local>
2024-09-11 12:24:27 +0600

    Making changes on the client side for JWT token based authentication

--- token-based-authentication-jwt/bank-app-ui/src/app/components/login/login.component.ts
+++ token-based-authentication-jwt/bank-app-ui/src/app/components/login/login.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { User } from "src/app/model/user.model";
-import { NgForm } from '@angular/forms';
-import { LoginService } from 'src/app/services/login/login.service';
-import { Router } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {User} from "src/app/model/user.model";
+import {NgForm} from '@angular/forms';
+import {LoginService} from 'src/app/services/login/login.service';
+import {Router} from '@angular/router';
 import {getCookie} from "typescript-cookie";
 
 
@@ -17,7 +17,7 @@ export class LoginComponent implements OnInit {
 
   constructor(private loginService: LoginService, private router: Router) {
 
-   }
+  }
 
   ngOnInit(): void {
 
@@ -26,7 +26,8 @@ export class LoginComponent implements OnInit {
   validateUser(loginForm: NgForm) {
     this.loginService.validateLoginDetails(this.model).subscribe(
       responseData => {
-        this.model = <any> responseData.body;
+        window.sessionStorage.setItem("Authorization", responseData.headers.get('Authorization')!);
+        this.model = <any>responseData.body;
         this.model.authStatus = 'AUTH';
         window.sessionStorage.setItem("userdetails", JSON.stringify(this.model));
         let xsrf = getCookie("XSRF-TOKEN")!;
