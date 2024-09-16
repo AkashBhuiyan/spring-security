12cdef9f418e0325c24141eb67e7ee8cc2fb16da
Akash <akash.bhuiyan@gmail.com>
2024-09-16 14:46:17 +0600

    Integrate Keycloak for authentication and route protection
    - Added KeycloakService initialization in AppModule.
    - Implemented login, logout, and user session management in HeaderComponent.
    - Configured AuthKeyClockGuard to protect routes based on roles.
    - Integrated KeycloakJS and Keycloak-Angular libraries for PKCE flow.
    - Configured routing for protected dashboard, account, balance, loans, and cards pages with role-based access control.
    - Updated allowedCommonJsDependencies for keycloak dependencies.

--- keycloak-oauth2-authorization-server/bank-app-ui/src/app/app.module.ts
+++ keycloak-oauth2-authorization-server/bank-app-ui/src/app/app.module.ts
@@ -1,22 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS, HttpClientXsrfModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { ContactComponent } from './components/contact/contact.component';
-import { LoginComponent } from './components/login/login.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { LogoutComponent } from './components/logout/logout.component';
-import { NoticesComponent } from './components/notices/notices.component';
-import { AccountComponent } from './components/account/account.component';
-import { BalanceComponent } from './components/balance/balance.component';
-import { LoansComponent } from './components/loans/loans.component';
-import { CardsComponent } from './components/cards/cards.component';
-import { XhrInterceptor } from './interceptors/app.request.interceptor';
-import { AuthActivateRouteGuard } from './routeguards/auth.routeguard';
-import { HomeComponent } from './components/home/home.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {APP_INITIALIZER, NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule, HttpClientXsrfModule} from '@angular/common/http';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './components/header/header.component';
+import {ContactComponent} from './components/contact/contact.component';
+import {LoginComponent} from './components/login/login.component';
+import {DashboardComponent} from './components/dashboard/dashboard.component';
+import {LogoutComponent} from './components/logout/logout.component';
+import {NoticesComponent} from './components/notices/notices.component';
+import {AccountComponent} from './components/account/account.component';
+import {BalanceComponent} from './components/balance/balance.component';
+import {LoansComponent} from './components/loans/loans.component';
+import {CardsComponent} from './components/cards/cards.component';
+import {HomeComponent} from './components/home/home.component';
+import {KeycloakService} from 'keycloak-angular';
+
+
+function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      config: {
+        url: 'http://localhost:8180/',
+        realm: 'bankdev',
+        clientId: 'publicclient',
+      },
+      initOptions: {
+        pkceMethod: 'S256',
+        redirectUri: 'http://localhost:4200/dashboard',
+      },loadUserProfileAtStartUp: false
+    });
+}
 
 @NgModule({
   declarations: [
@@ -45,10 +60,11 @@ import { HomeComponent } from './components/home/home.component';
   ],
   providers: [
     {
-      provide : HTTP_INTERCEPTORS,
-      useClass : XhrInterceptor,
-      multi : true
-    },AuthActivateRouteGuard
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService],
+    }
   ],
   bootstrap: [AppComponent]
 })
